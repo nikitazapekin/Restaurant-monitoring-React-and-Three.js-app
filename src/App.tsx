c12aee4124be556468b851c86b9a1e09d1dfef8a
nikitazapekin <wotblitz362@mail.ru>
2024-02-17 17:11:28 +0300

    restaurant

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import AboutPage from "./pages/aboutPage/aboutPage";
 import RestaurantPage from "./pages/restaurantPage/restaurantPage";
+ 
 //import { Footer, Navbar } from "./components";
 //import { About, Contact, Home, Projects } from "./pages";
 
@@ -33,3 +34,13 @@ const App = () => {
 };
 
 export default App;
+//https://sketchfab.com/3d-models/restaurant-f8f8fb8c0c3549fa81802b36467be157
+//https://sketchfab.com/3d-models/dna-alleys-restaurant-f35558b5f234433abb4ca45869dc6338
+//https://sketchfab.com/3d-models/japanese-restaurant-lowpoly-e59abe484f9f4cbba3dc45f27485b223
+//https://sketchfab.com/3d-models/low-poly-restaurant-e4326bf6f2324ccf828ce123871c7952
+//https://sketchfab.com/3d-models/restaurant-in-golf-club-hampshire-318dd20c6abb4c119b6289da41dbafcd   много столов
+
+// https://sketchfab.com/3d-models/restaurant-f8f8fb8c0c3549fa81802b36467be157 оптимизированная  и норм выглядит
+
+
+//npx gltfjsx scene.gltf
--- src/components/restaurantPreview/restaurantPreview.jsx
+++ src/components/restaurantPreview/restaurantPreview.jsx
@@ -100,7 +100,7 @@ const RestaurantPreview = () => {
  
 export default RestaurantPreview;  */
 
-
+/*
 import React, { useRef, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import Music from "../../assets/sakura.mp3";
@@ -161,12 +161,15 @@ const RestaurantPreview = () => {
   );
 };
 export default RestaurantPreview;
-/*
+*/
+
+
 import React, { useRef, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import Music from "../../assets/sakura.mp3";
 import { Suspense } from "react";
-import { Sky } from "@react-three/drei";
+import { Sky } from "../../models";
+import Restaurant from "../../models/Restaurant";
 
 const RestaurantPreview = () => {
   const audioRef = useRef(new Audio(Music));
@@ -175,6 +178,7 @@ const RestaurantPreview = () => {
 
   const [isRotating, setIsRotating] = useState(false);
   const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+  const rotationSpeed = isRotating ? 0.0000000000001 : 0; // Adjust rotation speed here
 
   useEffect(() => {
     if (isPlayingMusic) {
@@ -192,76 +196,10 @@ const RestaurantPreview = () => {
     setIsRotating(!isRotating);
   };
 
-  return (
-    <>
-      <section className="w-full h-screen relative">
-        <button onClick={toggleSound}>Sound</button>
-        <button onClick={toggleRotation}>
-          {isRotating ? "Stop Rotation" : "Start Rotation"}
-        </button>
-
-        <Canvas
-          style={{ width: "100wv", height: "100vh" }}
-          camera={{ near: 0.1, far: 1000 }}
-        >
-          <Suspense fallback={"loading"}>
-            <directionalLight position={[1, 1, 1]} intensity={2} />
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 5, 10]} intensity={2} />
-            <spotLight
-              position={[0, 50, 10]}
-              angle={0.15}
-              penumbra={1}
-              intensity={2}
-            />
-            <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
-            <Sky rotation={isRotating ? 0.5 : 0} />
-          </Suspense>
-        </Canvas>
-      </section>
-    </>
-  );
-};
-
-export default RestaurantPreview; */
 
 
 
-
-
-
-
-
-
-
-/*
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
-
-import sakura from "../../assets/sakura.mp3";
-//import { HomeInfo, Loader } from "../components";
-//import { soundoff, soundon } from "../assets/icons";
-//import { Sky } from "../models";
-import { Sky } from "../../models";
-const  RestaurantPreview = () => {
-  const audioRef = useRef(new Audio(sakura));
-  audioRef.current.volume = 0.4;
-  audioRef.current.loop = true;
-
-  const [currentStage, setCurrentStage] = useState(1);
-  const [isRotating, setIsRotating] = useState(false);
-  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
-
-  useEffect(() => {
-    if (isPlayingMusic) {
-      audioRef.current.play();
-    }
-
-    return () => {
-      audioRef.current.pause();
-    };
-  }, [isPlayingMusic]);
-
+ 
   const adjustBiplaneForScreenSize = () => {
     let screenScale, screenPosition;
 
@@ -281,10 +219,12 @@ const  RestaurantPreview = () => {
     let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
+    //  screenScale = [0.9, 0.9, 0.9];
+    screenScale = [2, 2, 2];
       screenPosition = [0, -6.5, -43.4];
     } else {
-      screenScale = [1, 1, 1];
+     // screenScale = [1, 1, 1];
+     screenScale = [2, 2, 2];
       screenPosition = [0, -6.5, -43.4];
     }
 
@@ -293,44 +233,46 @@ const  RestaurantPreview = () => {
 
   const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
   const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [currentStage, setCurrentStage] = useState(10);
+
 
   return (
-    <section className='w-full h-screen relative'>
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-   
-      </div>
+    <>
+      <section className="w-full h-screen relative">
+        <Canvas
+          style={{ width: "100wv", height: "100vh" }}
+          camera={{ near: 0.1, far: 1000 }}
+        >
+          <Suspense fallback={"loading"}>
+            <directionalLight position={[1, 1, 1]} intensity={2} />
+            <ambientLight intensity={0.5} />
+            <pointLight position={[10, 5, 10]} intensity={2} />
+            <spotLight
+              position={[0, 50, 10]}
+              angle={0.15}
+              penumbra={1}
+              intensity={2}
+            />
+            <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+            <Sky isRotating={rotationSpeed !== 0} rotationSpeed={0.0001} />
 
-      <Canvas
-        className={`w-full h-screen bg-transparent ${
-          isRotating ? "cursor-grabbing" : "cursor-grab"
-        }`}
-        camera={{ near: 0.1, far: 1000 }}
-      >
-        <Suspense fallback={"load"}>
-          <directionalLight position={[1, 1, 1]} intensity={2} />
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 5, 10]} intensity={2} />
-          <spotLight
-            position={[0, 50, 10]}
-            angle={0.15}
-            penumbra={1}
-            intensity={2}
-          />
-          <hemisphereLight
-            skyColor='#b1e1ff'
-            groundColor='#000000'
-            intensity={1}
-          />
-
-          <Sky isRotating={isRotating} />
-   
-        </Suspense>
-      </Canvas>
-
-   
-    </section>
+            <Restaurant 
+            isRotating={rotationSpeed !== 0}
+             rotationSpeed={0.0001}
+         
+            //  setCurrentStage={setCurrentStage}
+              position={islandPosition}
+              rotation={[0.1, 4.7077, 0]}
+              scale={islandScale}
+            />
+          </Suspense>
+        </Canvas>
+        <button onClick={toggleSound}>Sound</button>
+        <button onClick={toggleRotation}>
+          {isRotating ? "Stop Rotation" : "Start Rotation"}
+        </button>
+      </section>
+    </>
   );
 };
-
 export default RestaurantPreview;
- */
